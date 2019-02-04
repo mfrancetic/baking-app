e509b779a33748fa72e41b6679fda6af78f22b07
Maja Francetic <francetic.maja@gmail.com>
2019-02-04 23:31:04 +0100

    Commit before migration

--- app/src/androidTest/java/com/example/android/bakingapp/MainActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/bakingapp/MainActivityBasicTest.java
@@ -1,5 +1,7 @@
 package com.example.android.bakingapp;
 
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.IdlingResource;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -11,11 +13,15 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertNotNull;
 
 import com.example.android.bakingapp.activities.MainActivity;
+import com.example.android.bakingapp.idlingResource.SimpleIdlingResource;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,9 +29,19 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityBasicTest {
 
+    private IdlingResource idlingResource;
+
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void registerIdlingResource() {
+        idlingResource = activityTestRule.getActivity().getIdlingResource();
+        IdlingRegistry.getInstance().register(idlingResource);
+        getInstrumentation().waitForIdleSync();
+
+    }
 
     @Test
     public void openMainActivity_displaysRecipeList() {
@@ -34,9 +50,17 @@ public class MainActivityBasicTest {
 
     @Test
     public void clickRecyclerViewItem_displaysDetailActivity() {
+
         onView(withId(R.id.main_recycler_view)).perform(RecyclerViewActions.
                 actionOnItemAtPosition(0, click()));
+//        getInstrumentation().waitForIdleSync();
 
+    }
 
+    @After
+    public void unregisterIdlingResource() {
+        if (idlingResource != null) {
+            IdlingRegistry.getInstance().unregister(idlingResource);
+        }
     }
 }
